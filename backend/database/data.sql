0f7dd00b9d0bd9534a1535b5f4648909f70f6dd2
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-03-29 20:24:21 -0300

    data.sql standard data inserts syntax is fixed

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -2,31 +2,31 @@ CREATE DATABASE IF NOT EXISTS TASKOPIA;
 
 USE TASKOPIA;
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
                       username VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE boards(
+CREATE TABLE IF NOT EXISTS boards(
                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
                        user_id BIGINT,
                        FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE task_lists(
+CREATE TABLE IF NOT EXISTS task_lists(
                            id BIGINT PRIMARY KEY AUTO_INCREMENT,
                            board_id BIGINT,
                            title VARCHAR(255) NOT NULL,
-                           positionInBoard INT NOT NULL,
+                           position_in_board INT NOT NULL,
                            FOREIGN KEY (board_id) REFERENCES boards(id)
 );
 
-CREATE TABLE tasks(
+CREATE TABLE IF NOT EXISTS tasks(
                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
                       task_list_id BIGINT,
                       title VARCHAR(255) NOT NULL,
                       description VARCHAR(255) NOT NULL,
-                      positionInTaskList INT NOT NULL,
+                      position_in_task_list INT NOT NULL,
                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                       done_at TIMESTAMP,
                       FOREIGN KEY (task_list_id) REFERENCES task_lists(id)
