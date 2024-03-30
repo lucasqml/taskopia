be49fd4e511f10e93d133081e724ba717a3cf42f
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-03-29 22:47:35 -0300

    Backend CORS allow flag is added to controllers

--- backend/src/main/kotlin/com/example/taskopia/controller/BoardController.kt
+++ backend/src/main/kotlin/com/example/taskopia/controller/BoardController.kt
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/board")
+@CrossOrigin(origins = ["http://localhost:3000"])
 class BoardController(private val boardRepository: BoardRepository) {
 
     @GetMapping("/{userId}")
-    fun getBoardByUserId(@PathVariable userId: Long): Board? = boardRepository.findByUserId(userId)
+    fun getBoardByUserId(@PathVariable userId: Long): Board? {
+        print(message = "userId: $userId")
+        return boardRepository.findByUserId(userId)
+    }
 
     @PostMapping("")
     fun createBoard(@RequestBody board: Board): Board = boardRepository.save(board)
