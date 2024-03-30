f3734165b491e6bf4c221710b0aad4f480a40b12
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-03-30 18:47:12 -0300

    BoardProvider Actions Handler is extracted

--- client/app/providers/implementations/current-board-provider/http-board-provider.tsx
+++ client/app/providers/implementations/current-board-provider/http-board-provider.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
 import { TaskopiaAPI } from "..";
-import { CreateTaskInput, CurrentBoardProvider, CurrentUserProvider } from "../../interfaces";
+import {
+  CreateTaskInput,
+  CurrentBoardProvider,
+  CurrentUserProvider,
+} from "../../interfaces";
 import { QueryOf } from "@/app/types/query";
 import { Board, Task } from "@/app/types";
-
-type AddTaskAction = {
-  type: "ADD_TASK";
-  actionResult: Task
-  actionInput: CreateTaskInput 
-}
-
-type BoardAction = AddTaskAction
+import { BoardProviderActionsHandler } from "./board-provider-actions-handler";
 
 export function HttpBoardProvider(
   boardAPI: TaskopiaAPI,
@@ -21,21 +18,22 @@ export function HttpBoardProvider(
     data: undefined,
     error: undefined,
   });
-
-  const [actionsQueue, setActionsQueue] = useState<BoardAction[]>([])
-
   const currentUser = currentUserProvider.currentUser().data;
 
+  const { addActionToQueue } = BoardProviderActionsHandler({
+    boardAPI,
+    currentBoardQuery,
+    setBoardQueryState: setCurrentBoardQuery,
+  });
+
   useEffect(() => {
-    async function fetchCurrentBoard(
-      currentUserId: string
-    ) {
+    async function fetchCurrentBoard(currentUserId: string) {
       try {
         setCurrentBoardQuery({
           isLoading: true,
           data: undefined,
-          error: undefined
-        })
+          error: undefined,
+        });
 
         const board = await boardAPI.getBoard(currentUserId);
         setCurrentBoardQuery({
@@ -63,7 +61,7 @@ export function HttpBoardProvider(
 
     const newTask: Task = {
       // generate fake id
-      id: Math.random().toString(36).substring(7),
+      id: "fake" + Math.random().toString(36).substring(7),
       title: task.title,
       description: task.description,
       tags: [],
@@ -81,7 +79,7 @@ export function HttpBoardProvider(
           };
         }
         return taskList;
-      }) 
+      }),
     };
 
     setCurrentBoardQuery({
@@ -90,85 +88,17 @@ export function HttpBoardProvider(
       error: null,
     });
 
-
-    const action: AddTaskAction = {
+    addActionToQueue({
       type: "ADD_TASK",
       actionResult: newTask,
-      actionInput: task
-    }
-    setActionsQueue([
-      ...actionsQueue,
-      action
-    ])
-  }
-
-  async function processAddTaskAction(action: AddTaskAction) {
-    // actually send the request to the server
-    try {
-      const task = action.actionInput
-      const optimisticTask = action.actionResult
-      const createdTask = await boardAPI.postTask(task);
-      if (!currentBoardQuery.data){
-        throw new Error("No board loaded");
-      }
-      const board: Board = currentBoardQuery.data
-  
-      // update the task with the server response
-      const updatedBoard = {
-        ...currentBoardQuery.data,
-        taskLists: board.taskLists.map((taskList) => {
-          if (taskList.id === task.taskListId) {
-            return {
-              ...taskList,
-              tasks: taskList.tasks.map((task) => {
-                if (task.id === optimisticTask.id) {
-                  return createdTask;
-                }
-                return task;
-              }),
-            };
-          }
-          return taskList;
-        }),
-      };
-  
-      setCurrentBoardQuery({
-        isLoading: false,
-        data: updatedBoard,
-        error: null,
-      });
-    } catch (error: any) {
-      // TODO rollback the optimistic update
-      setCurrentBoardQuery({
-        isLoading: false,
-        data: undefined,
-        error: error,
-      });
-    }
-  }
-
-  // where the actions are actually sent to the server
-  useEffect(() => {
-    async function processActions() {
-      if (actionsQueue.length === 0) return
-
-      const action = actionsQueue[0]
-      switch (action.type) {
-        case "ADD_TASK":
-          await processAddTaskAction(action)
-          break
-      }
-
-      setActionsQueue(actionsQueue.slice(1))
-    }
-
-    processActions()
-  })
+      actionInput: task,
+    });
+  };
 
   return {
     currentBoard: () => {
       return currentBoardQuery;
     },
-    createTask: optimisticAddTask
+    createTask: optimisticAddTask,
   };
 }
