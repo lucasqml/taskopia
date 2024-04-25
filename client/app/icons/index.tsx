01175c4a52552d0405ede2f14ad442cb713233fd
Roberto Bolgheroni <robertobolgheroni@gmail.com>
2024-04-25 19:56:55 -0300

    ReactIcons is added

---
+++ client/app/icons/index.tsx
@@ -0,0 +1,9 @@
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+
+export function RightArrowIcon() {
+  return <FaAngleDoubleRight />;
+}
+
+export function LeftArrowIcon() {
+  return <FaAngleDoubleLeft />;
+}
