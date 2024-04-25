bb167200f8d9d1eaaa06c3cc72d8e177905867c7
Roberto Bolgheroni <robertobolgheroni@gmail.com>
2024-04-25 18:45:06 -0300

    MoveTask action is implemented in task components

--- client/app/components/task/task.component.tsx
+++ client/app/components/task/task.component.tsx
@@ -2,7 +2,7 @@ import { type Task } from "@/app/types";
 import { useState } from "react";
 import { TaskReadBody } from "./read-body";
 import { TaskEditBody } from "./edit-body";
-import { EditTaskInput } from "@/app/providers/interfaces";
+import { EditTaskInput, useCurrentBoardProvider } from "@/app/providers/interfaces";
 
 export type TaskProps = {
   task: Task;
@@ -11,13 +11,35 @@ export type TaskProps = {
 
 export function Task({ task, onEditTask }: TaskProps) {
   const [isEditing, setIsEditing] = useState(false);
+  const {
+    moveTask,
+    currentBoard
+  } = useCurrentBoardProvider();
 
-
+  function getNextTaskListId() {
+    const currentBoardData = currentBoard().data;
+    if (!currentBoardData) {
+      throw new Error('Board not found');
+    }
+    const sortedTaskLists = currentBoardData.taskLists.sort((a, b) => a.positionInBoard - b.positionInBoard);
+    const currentTaskListIndex = sortedTaskLists.findIndex(taskList => taskList.id === task.taskListId);
+    const nextTaskList = sortedTaskLists[currentTaskListIndex + 1];
+    return nextTaskList.id;
+  }
   return (
     <li
       key={task.id}
       className="flex flex-col justify-between gap-1 min-h-20 bg-gray-100 p-2 pt-3 rounded"
     >
+      <button
+        className="bg-blue-500 text-white rounded p-1"
+        onClick={() => moveTask({
+          taskId: task.id,
+          originTaskListId: task.taskListId,
+          destinationTaskListId: getNextTaskListId(),
+          positionInList: task.positionInList + 1
+        })}
+      >Move</button>
       {!isEditing && (
         <TaskReadBody
           task={task}
