29c74b659ebec0a64d54446aeaaabd5464612bec
Roberto Bolgheroni <robertobolgheroni@gmail.com>
2024-05-16 08:41:58 -0300

    TaskList Create form is created
    Signed-off-by: Roberto Bolgheroni <robertobolgheroni@gmail.com>

---
+++ client/app/components/create-task-list-form.component.tsx
@@ -0,0 +1,23 @@
+type CreateTaskListFormComponentProps = {
+  onCreateList: (listName: string) => void;
+};
+
+export function CreateTaskListFormComponent({
+  onCreateList,
+}: CreateTaskListFormComponentProps) {
+  return (
+    <div
+      aria-label="Add List Section"
+      className="w-72 h-min bg-gray-200 rounded-lg p-4 pt-0"
+    >
+      <input
+        type="text"
+        placeholder="List Name"
+        className="w-full mt-4 p-2 border border-gray-300 rounded-md"
+      />
+      <button className="w-full mt-4 p-2 bg-blue-500 text-white rounded-md">
+        Add List
+      </button>
+    </div>
+  );
+}
